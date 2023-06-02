e500e9c35100c9adc974ce0b0c6c270fe0629ba5
juliaoliveiramachado <133999730+juliaoliveiramachado@users.noreply.github.com>
2023-06-01 22:29:16 -0300

    Update grafico.py

--- grafico.py
+++ grafico.py
@@ -1,21 +1,10 @@
 # atividade2
-pip install matplotlib
-
-import pandas as pd
-import matplotlib.pyplot as plt
 import streamlit as st
+import pandas as pd
+import numpy as np
 
-df = pd.read_csv('nascidosvivos_sexo_anoatual_distritos.csv')
+chart_data = pd.DataFrame(
+    np.random.randn(20, 3),
+    columns=["feminino", "masculino"])
 
-total_masculino = df['masculino'].sum()
-total_feminino = df['feminino'].sum()
-
-contagens = [total_masculino, total_feminino]
-
-labels = ['Masculino', 'Feminino']
-
-fig, ax = plt.subplots()
-ax.pie(contagens, labels=labels, autopct='%1.1f%%')
-ax.set_title('Distribuição de Nascidos por Sexo')
-
-st.pyplot(fig)
+st.bar_chart(chart_data)
