4da52e2cc3bf0423ef94ccadbdd02498ca4f6f98
juliaoliveiramachado <133999730+juliaoliveiramachado@users.noreply.github.com>
2023-06-01 22:33:06 -0300

    Update grafico.py

--- grafico.py
+++ grafico.py
@@ -4,7 +4,8 @@ import pandas as pd
 import numpy as np
 
 chart_data = pd.DataFrame(
-    np.random.randn(20, 3),
+    np.random.randn(20, 2),
     columns=["feminino", "masculino"])
 
 st.bar_chart(chart_data)
+
